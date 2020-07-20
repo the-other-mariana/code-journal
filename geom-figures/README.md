97e7041a25fea95f98158905f7521f5fe0d238c7
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-20 17:13:42 -0500

    updated readme

---
+++ geom-figures/README.md
@@ -0,0 +1,13 @@
+# Geometric Figure Spiral
+
+This is a small script that creates a series of regular polygons to form a spiral.<br />
+
+## Specifications
+
+Python version: `3.6.2` <br />
+
+## Output
+
+For an hexagon, a sample output I had was the following: <br />
+
+![alt text](https://github.com/the-other-mariana/code-journal/blob/master/geom-figures/results/hexagon03.png?raw=true) <br />
--- geom-figures/geom-figs.py
+++ geom-figures/geom-figs.py
@@ -25,38 +25,45 @@ h = len(img)
 w = len(img[0])
 
 num_sides = 6
+fig_center = [0.0, 0.0]
 step_angle = 360.0 / num_sides
 size = 60
 PI = 3.1416
-delta_angle = 5.0
-num_iter = 10
+delta_angle = 45.0
+delta_size = 10.0
+num_iter = 5
 
+acc_angle = 0.0
+acc_size = 0.0
+for j in range(num_iter):
+	last_point = [0.0, 0.0]
+	first_point = [0.0, 0.0]
+	acc_angle += delta_angle
+	acc_size += delta_size
+	for i in range(num_sides):
 
-last_point = [0.0, 0.0]
-first_point = [0.0, 0.0]
-for i in range(num_sides):
-	angle = i * step_angle - (step_angle / 2.0)
-	x = size * math.cos(angle * PI / 180.0)
-	y = size * math.sin(angle * PI / 180.0)
+		angle = i * step_angle - (step_angle / 2.0) + acc_angle
+		x = fig_center[0] + (size + acc_size) * math.cos(angle * PI / 180.0)
+		y = fig_center[1] + (size + acc_size) * math.sin(angle * PI / 180.0)
 
-	if i == 0:
-		first_point = [x, y]
-	if i > 0:
-		ln.line([x, y], last_point, thick, color, img)
-	if i == num_sides - 1:
-		ln.line([x, y], first_point, thick, color, img)
+		if i == 0:
+			first_point = [x, y]
+		if i > 0:
+			ln.line([x, y], last_point, thick, color, img)
+		if i == num_sides - 1:
+			ln.line([x, y], first_point, thick, color, img)
 
-	last_point = [x, y]
+		last_point = [x, y]
 
-	x += w / 2.0
-	y = h / 2.0 - y
+		x += w / 2.0
+		y = h / 2.0 - y
 
-	x = int(x)
-	y = int(y)
+		x = int(x)
+		y = int(y)
 
-	# validate if point is inside image
-	if x > w or y > h or x < 0 or y < 0:
-		continue
+		# validate if point is inside image
+		if x > w or y > h or x < 0 or y < 0:
+			continue
 
 # show image
 plt.plot(), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
