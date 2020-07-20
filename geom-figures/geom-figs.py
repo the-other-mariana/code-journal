012b0924c3deffa15f2e0bfd1c86642c7a1dd5bb
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-20 16:49:16 -0500

    bug fix: remove vertex array

--- geom-figures/geom-figs.py
+++ geom-figures/geom-figs.py
@@ -11,9 +11,10 @@ img = np.zeros([img_size, img_size,3],dtype=np.uint8)
 img[:] = (255, 255, 255)
 
 # painting a red line
-color = [0, 0, 255]
+color = [0, 0, 0]
 gridGap = 20
 thick = 2
+
 
 #ln.grid(gridGap, img) # paints grid
 #ln.line(p1, p2, thickness, color, img)
@@ -25,24 +26,27 @@ w = len(img[0])
 
 num_sides = 6
 step_angle = 360.0 / num_sides
-size = 40
+size = 60
 PI = 3.1416
+delta_angle = 5.0
+num_iter = 10
 
 
 last_point = [0.0, 0.0]
-verts = []
+first_point = [0.0, 0.0]
 for i in range(num_sides):
 	angle = i * step_angle - (step_angle / 2.0)
 	x = size * math.cos(angle * PI / 180.0)
 	y = size * math.sin(angle * PI / 180.0)
 
+	if i == 0:
+		first_point = [x, y]
 	if i > 0:
 		ln.line([x, y], last_point, thick, color, img)
 	if i == num_sides - 1:
-		ln.line([x, y], verts[0], thick, color, img)
+		ln.line([x, y], first_point, thick, color, img)
 
 	last_point = [x, y]
-	verts.append(last_point)
 
 	x += w / 2.0
 	y = h / 2.0 - y
@@ -53,8 +57,6 @@ for i in range(num_sides):
 	# validate if point is inside image
 	if x > w or y > h or x < 0 or y < 0:
 		continue
-
-print(verts)
 
 # show image
 plt.plot(), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
