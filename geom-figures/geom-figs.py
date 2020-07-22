90df211147099bdc6e44beac4010ef2b0f240c44
Mariana Avalos <mariana.avalos.arce@gmail.com>
2020-07-21 20:31:03 -0500

    increasing size, constant angle

--- geom-figures/geom-figs.py
+++ geom-figures/geom-figs.py
@@ -24,27 +24,32 @@ thick = 2
 h = len(img)
 w = len(img[0])
 
-num_sides = 6
+num_sides = 4
 fig_center = [0.0, 0.0]
 step_angle = 360.0 / num_sides
 size = 60
 PI = 3.1416
-delta_angle = 45.0
-delta_size = 10.0
-num_iter = 5
+num_iter = 12
 
+angle_dist = 2
+delta_angle = (angle_dist * 180.0 / (num_sides)) / num_iter
+#delta_angle = 15.0
+print(delta_angle)
+delta_size = 5.0
 acc_angle = 0.0
-acc_size = 0.0
+acc_size = size
+dist = 0.0
+verts = []
 for j in range(num_iter):
 	last_point = [0.0, 0.0]
 	first_point = [0.0, 0.0]
-	acc_angle += delta_angle
-	acc_size += delta_size
+
 	for i in range(num_sides):
 
-		angle = i * step_angle - (step_angle / 2.0) + acc_angle
-		x = fig_center[0] + (size + acc_size) * math.cos(angle * PI / 180.0)
-		y = fig_center[1] + (size + acc_size) * math.sin(angle * PI / 180.0)
+		angle = i * step_angle - (step_angle / 2.0) - acc_angle
+		x = fig_center[0] + (acc_size) * math.cos(angle * PI / 180.0)
+		y = fig_center[1] + (acc_size) * math.sin(angle * PI / 180.0)
+
 
 		if i == 0:
 			first_point = [x, y]
@@ -53,8 +58,9 @@ for j in range(num_iter):
 		if i == num_sides - 1:
 			ln.line([x, y], first_point, thick, color, img)
 
-		last_point = [x, y]
 
+		last_point = [x, y]
+		verts.append([x, y])
 		x += w / 2.0
 		y = h / 2.0 - y
 
@@ -64,6 +70,22 @@ for j in range(num_iter):
 		# validate if point is inside image
 		if x > w or y > h or x < 0 or y < 0:
 			continue
+		'''
+		if j > 0 and i == num_sides - 1:
+			dist = math.sqrt((verts[i + (j*num_sides)][0] - verts[i + (j*num_sides) - num_sides][0])**2 + (verts[i + (j*num_sides)][1] - verts[i + (j*num_sides) - num_sides][1])**2)
+			print(str(i + (j*num_sides)), str(i + (j*num_sides) - num_sides))
+			ln.line(verts[i + (j*num_sides)], verts[i + (j*num_sides) - num_sides], thick, [0, 0, 255], img)
+			print(dist)
+		'''
+		ca = (acc_size * math.sin(delta_angle * PI / 180.0)) / (math.tan((step_angle / 2.0) * PI / 180.0))
+		ca = abs(acc_size - ca)
+		print(ca)
+	acc_angle += delta_angle
+	#acc_size += delta_size
+	#acc_size += dist * math.cos((step_angle/2.0) * PI / 180.0)
+
+	acc_size += abs(acc_size - ca)
+
 
 # show image
 plt.plot(), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
