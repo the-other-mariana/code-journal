bd169f9278141bec59b67a9d783c052d288d9112
Mariana Avalos <mariana.avalos.arce@gmail.com>
2021-01-02 10:58:06 -0600

    updated solution

--- longest-unique-string/solution.py
+++ longest-unique-string/solution.py
@@ -5,19 +5,27 @@ str = input().lower()
 visited = []
 
 for i in range(26):
+    # counter, times, start, end
     visited.append([0, 1])
-
+start = 0
+end = 0
 max = 0
 length = 0
 for i in range(len(str)):
     c = str[i]
     visited[ord(c) - 97][0] += 1
     if visited[ord(c) - 97][0] <= visited[ord(c) - 97][1]:
+        end += 1
         length += 1
     if visited[ord(c) - 97][0] > visited[ord(c) - 97][1]:
         visited[ord(c) - 97][1] += 1
         length = 1
+        for j in range(start, end):
+            visited[ord(str[j]) - 97][1] = visited[ord(c) - 97][1]
+        start = i
+        end = start
     if length > max:
         max = length
+    print(c, length, max, visited)
 
 print(str, max)
